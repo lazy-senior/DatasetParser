45d7455609cdf3520f6ab2a251898f4467833887
Vinzenz <lazy-senior@users.noreply.github.com>
2023-12-20 19:02:59 +0100

    Prüfen in welchen Formularen das Dataset genutzt wird

--- DatasetParser.Core/Proalpha/Service/DatasetPlausabilityService.cs
+++ DatasetParser.Core/Proalpha/Service/DatasetPlausabilityService.cs
@@ -1,4 +1,8 @@
 using DatasetParser.Core.Proalpa.Database;
+using DatasetParser.Core.Proalpa.Database.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
 using Proalpha.Dataset;
 
 namespace DatasetParser.Core.Proalpa.Service
@@ -18,14 +22,36 @@ namespace DatasetParser.Core.Proalpa.Service
 
             Dataset dataset = new();
             dataset.Parse(filePath);
-            
-            Console.WriteLine($"Informationen zu Dataset:{filePath}");
-            Console.WriteLine("---");
             PlausibilityCheck(dataset);
         }
 
         private void PlausibilityCheck(Dataset dataset){
             
+            var drc_Dataset = ProalphaContext.DRC_Datasets.ToList()
+                .FirstOrDefault(drc_dataset => drc_dataset.DRC_Dataset_ID == dataset.DatasetName);
+
+            var DRC_DataProviders = ProalphaContext.DRC_DataProviders
+                .Where(DRC_DataProvider => DRC_DataProvider.DRC_Dataset_Obj == drc_Dataset.DRC_Dataset_Obj)
+                .Join(
+                    ProalphaContext.BG_Kopf,
+                    drc_DataProvider => drc_DataProvider.Owning_Obj,
+                    bg_Kopf => bg_Kopf.BG_Kopf_Obj,
+                    (drc_DataProvider, bg_Kopf) => new {drc_DataProvider, bg_Kopf}
+                );
+            
+
+    
+            Console.WriteLine($"Informationen zu Dataset:{dataset.DatasetName}");
+            Console.WriteLine("---");
+            Console.WriteLine($"DRC_Dataset_ID\t\t:{drc_Dataset.DRC_Dataset_ID}");
+            Console.WriteLine($"DatasetDefinitionFile\t:{drc_Dataset.DatasetDefinitionFile}");
+            Console.WriteLine($"MasterDataValidation\t:{drc_Dataset.MasterDataValidation}");
+            Console.WriteLine("---");
+            foreach(var a in DRC_DataProviders){
+                
+                Console.WriteLine($"Formular:{a.bg_Kopf.Formular}");
+            }
+            Console.WriteLine("---");
             Console.WriteLine($"TempTable-Includes\t:{dataset.TempTables.Count()}");
             Console.WriteLine($"TempTable-Defines\t:{dataset.DefinedTempTables.Count()}");
             Console.WriteLine($"TempTable-Relationships\t:{dataset.DataRelations.Count()}");
